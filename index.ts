3ac4d30fa733fd747307cd77f29a5ce8c0988111
hisanori <hisary@gmail.com>
2025-07-21 16:45:57 +0900

    fix export

--- index.ts
+++ index.ts
@@ -1,10 +1,5 @@
-import { printJsonFilesDiff, diffJsonFiles } from './lib/diff-files.js';
-import { toPathJqQuery } from './lib/path-utils.js';
-import { diffJsonValues } from './lib/diff.js';
+export { printJsonFilesDiff, diffJsonFiles } from './lib/diff-files.js';
+export { toPathJqQuery } from './lib/path-utils.js';
+export { diffJsonValues } from './lib/diff.js';
 
-export {
-  printJsonFilesDiff,
-  diffJsonFiles,
-  toPathJqQuery,
-  diffJsonValues,
-};
+export { type DiffItem } from './lib/types.js';

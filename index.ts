10947ceed7803b0663a290366861773530678f37
hisanori <hisary@gmail.com>
2025-07-27 16:30:24 +0900

    remove an unintended exporting

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 export { printJsonFilesDiff, diffJsonFiles } from './lib/diff-files.js';
-export { toPathJqQuery } from './lib/path-utils.js';
 export { diffJsonValues } from './lib/diff.js';
 
 export { type DiffItem } from './lib/types.js';
